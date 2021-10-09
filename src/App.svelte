5136bd2c751666bac535ca363c6483f6c8287c03
babyfacedeveloper <babyfacedeveloper@gmail.com>
2021-10-09 11:40:23 +0700

    Scaffold template

--- src/App.svelte
+++ src/App.svelte
@@ -1,76 +1,25 @@
 <script lang="ts">
-	export let name: string;
+	import { Route, Router } from "svelte-navigator";
+	import Navbar from "./components/Navbar.svelte";
+	import Home from "./Home.svelte";
+	import NotFound from "./NotFound.svelte";
+
+	// export let name: string;
 </script>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-	<div class="container-fluid">
-		<a class="navbar-brand" href="#">Navbar</a>
-		<button
-			class="navbar-toggler"
-			type="button"
-			data-bs-toggle="collapse"
-			data-bs-target="#navbarNav"
-			aria-controls="navbarNav"
-			aria-expanded="false"
-			aria-label="Toggle navigation"
-		>
-			<span class="navbar-toggler-icon" />
-		</button>
-		<div class="collapse navbar-collapse" id="navbarNav">
-			<ul class="navbar-nav ms-auto">
-				<li class="nav-item">
-					<a class="nav-link active" aria-current="page" href="#"
-						>Home</a
-					>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="#">Features</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="#">Pricing</a>
-				</li>
-				<li class="nav-item">
-					<a
-						class="nav-link disabled"
-						href="#"
-						tabindex="-1"
-						aria-disabled="true">Disabled</a
-					>
-				</li>
-			</ul>
+<Router>
+	<div class="d-flex flex-column h-100">
+		<Navbar />
+		<div class="container-fluid h-100">
+			<Route path="/">
+				<Home />
+			</Route>
+			<Route path="*">
+				<NotFound />
+			</Route>
 		</div>
 	</div>
-</nav>
-
-<main>
-	<h1>Hello {name}!</h1>
-	<p>
-		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
-		how to build Svelte apps.
-	</p>
-</main>
+</Router>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-	.navbar {
-		background-color: transparent !important;
-	}
 </style>
---
+++ src/components/Navbar.svelte
@@ -0,0 +1,48 @@
+<script>
+    import { link } from "svelte-navigator";
+</script>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="/" use:link>Svelte Projects</a>
+        <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+        >
+            <span class="navbar-toggler-icon" />
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a
+                        class="nav-link active"
+                        aria-current="page"
+                        href="/"
+                        use:link
+                    >
+                        Home
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="library" use:link> Library </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="calculator" use:link>
+                        Calculator
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<style>
+    .navbar {
+        background-color: transparent !important;
+    }
+</style>
