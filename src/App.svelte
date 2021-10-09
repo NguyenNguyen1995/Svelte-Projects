d8abb0a4e80e2c2e696d28749110e77d3eadf738
babyfacedeveloper <babyfacedeveloper@gmail.com>
2021-10-09 09:13:37 +0700

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,76 @@
+<script lang="ts">
+	export let name: string;
+</script>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	<div class="container-fluid">
+		<a class="navbar-brand" href="#">Navbar</a>
+		<button
+			class="navbar-toggler"
+			type="button"
+			data-bs-toggle="collapse"
+			data-bs-target="#navbarNav"
+			aria-controls="navbarNav"
+			aria-expanded="false"
+			aria-label="Toggle navigation"
+		>
+			<span class="navbar-toggler-icon" />
+		</button>
+		<div class="collapse navbar-collapse" id="navbarNav">
+			<ul class="navbar-nav ms-auto">
+				<li class="nav-item">
+					<a class="nav-link active" aria-current="page" href="#"
+						>Home</a
+					>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Features</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Pricing</a>
+				</li>
+				<li class="nav-item">
+					<a
+						class="nav-link disabled"
+						href="#"
+						tabindex="-1"
+						aria-disabled="true">Disabled</a
+					>
+				</li>
+			</ul>
+		</div>
+	</div>
+</nav>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+		how to build Svelte apps.
+	</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+	.navbar {
+		background-color: transparent !important;
+	}
+</style>
