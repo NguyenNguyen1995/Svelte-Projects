cc5f2d1c6fd80fc62d108c9353186cc8d4ba6d32
babyfacedeveloper <babyfacedeveloper@gmail.com>
2021-10-11 22:05:39 +0700

    scaffold project etch a sketch

---
+++ src/pages/projects/EtchASketch.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+    <title>Svelte Projects - Etch A Sketch</title>
+</svelte:head>
+
+<h2 class="text-center mb-3">Etch a Sketch</h2>
+
+<div class="w-100 d-flex justify-content-center mb-3">
+    <button class="btn btn-danger">Clear</button>
+</div>
